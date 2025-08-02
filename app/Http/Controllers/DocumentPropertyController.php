d5a2defda1507c80aacee075ea3a70ad30b94c2e
cristian <cristianstiven49@gmail.com>
2025-08-02 07:42:47 -0500

    ultimo

--- app/Http/Controllers/DocumentPropertyController.php
+++ app/Http/Controllers/DocumentPropertyController.php
@@ -12,13 +12,13 @@ class DocumentPropertyController extends Controller
     public function documentos(Request $request)
     {
         $request->validate([
-            'id_propiedad' => 'required|exists:owner_register_properties,id_propiedad',
             'archivo'      => 'required|file|mimes:jpg,jpeg,png,pdf,docx,zip|max:10240',
             'descripcion'  => 'nullable|string',
         ]);
 
         $file = $request->file('archivo');
-        $path = $file->store('public/archivos');
+        $path = $request->file('archivo')->store('archivos', 'public');
+
 
         $archivo = document_property::create([
             'id_propiedad'     => $request->id_propiedad,
